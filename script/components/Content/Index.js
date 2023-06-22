3482e3a134b47e1eb41be31e4420ebc88b130859
Sascha Seibel <sascha.seibel@outlook.de>
2023-06-22 10:44:35 +0900

    Create Index.js

---
+++ script/components/Content/Index.js
@@ -0,0 +1,9 @@
+import CreateCustomElement from "../../helper/CustomComponent.js";
+import { MainContent } from "./MainContent.js";
+import { SideInformation } from "./SideInformation.js";
+
+export const Content = CreateCustomElement("div", {
+    class: 'content-section'
+})
+
+Content.append(MainContent, SideInformation)
