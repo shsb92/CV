7ba616e9da23c13745526786c6e7465e72d4a048
Sascha Seibel <sascha.seibel@outlook.de>
2023-06-25 23:00:44 +0000

    updated content

--- script/content.js
+++ script/content.js
@@ -47,10 +47,10 @@ building robust web software solutions.
 
 Currently, as part of the development team at INFINITT Healthcare ltd., 
 I am actively involved in the development of the DoseM product. 
-This role has provided me with valuable opportunities to enhance my skills in JavaScript, 
+This role has provided me with valuable possibilities to enhance my skills in JavaScript, 
 React, and C#. Due to the nature of DoseM as a registered medical product in Germany, it 
 necessitated a strong focus on stability and responsiveness to customer feedback. 
-This gave the opportunity to further developed my flexibility in 
+This came along with the need to further develop my flexibility in 
 adapting to different software development environments.
 
 With a desire to build a solid foundation for my future in Korea, I am looking for opportunities 
@@ -70,7 +70,7 @@ export const Jobs = [
         During my time with the Dose팀 at INFINITT Healthcare, I played a crucial role in the 
         development of new features, refactoring existing components, and resolving bugs in both 
         the front-end and back-end of the DoseM application. This experience provided me with 
-        valuable opportunities to enhance my teamwork skills and adaptability in fast-paced work 
+        valuable opportunities to deepen my teamwork skills and adaptability in fast-paced work 
         environments. Given the high volume of customer requests, our team successfully demonstrated 
         the ability to promptly respond and cater to their needs.
         `,
@@ -84,7 +84,7 @@ export const Jobs = [
         title: "IT-Engineer ",
         sub_title: "INFINITT Healthcare GmbH - Frankfurt, Germany",
         description: `
-        Due to delays in the visa acquisition process, I had the opportunity to 
+        Due to delays in the visa acquisition process, I had the chance to 
         contribute as a helping hand at Infinitt Europe. During my time there, I focused on 
         enhancing my server administration skills and further developing my expertise in using 
         the Vue framework and C#. This experience allowed me to expand my professional skill 
@@ -99,8 +99,8 @@ export const Jobs = [
         title: "Scientific Researcher ",
         sub_title: "Justus Liebig University - Gießen, Germany",
         description: `
-            During my Master's studies, I had the opportunity to work at the DataIntegration Center 
-            Gießen. This experience allowed me to enhance my skillset as I contributed to the 
+            During my Master's studies, I had the privilege to work at the DataIntegration Center 
+            Gießen. This experience allowed me to improve my skillset as I contributed to the 
             development of multiple frontend applications. For example, I played a major role in developing 
             the frontend of a Research Data Management platform and implemented a robust solution for 
             pseudonymizing and anonymizing patient data through a dedicated trust office. Furthermore, 
